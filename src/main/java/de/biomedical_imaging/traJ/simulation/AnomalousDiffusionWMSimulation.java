9f5585c0af445f9dce95fc4671a0ab4a518e57dc
jumpfunky <thotho@posteo.de>
2016-06-21 09:44:49 +0200

    Remove unnecessary functions and comments.

--- src/main/java/de/biomedical_imaging/traJ/simulation/AnomalousDiffusionWMSimulation.java
+++ src/main/java/de/biomedical_imaging/traJ/simulation/AnomalousDiffusionWMSimulation.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import javax.vecmath.Point3d;
 
 import org.apache.commons.math3.ode.sampling.StepHandler;
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
 
 import de.biomedical_imaging.traJ.Trajectory;
 /*
@@ -35,16 +36,14 @@ public class AnomalousDiffusionWMSimulation extends AbstractSimulator {
 		t.add(new Point3d(0, 0, 0));
 		double[] incrx = generateIncrements();
 		double[] incry = generateIncrements();
-		double fact = Math.sqrt(diffusioncoefficient/1.36);
+	
 		for(int i = 1; i <= numberOfSteps; i++) {
-			//double steplength = Math.sqrt(incrx[i-1]*incrx[i-1] + incry[i-1]*incry[i-1]);
 			Point3d pos = new Point3d();
-			//Point3d pos2 = SimulationUtil.randomPosition(2, fact*steplength);
-			pos.setX(t.get(i-1).x + fact*incrx[i-1]);
-			pos.setY(t.get(i-1).y + fact*incry[i-1]);
+			pos.setX(t.get(i-1).x + incrx[i-1]); //Math.sqrt(2*diffusioncoefficient*timelag);
+			pos.setY(t.get(i-1).y + incry[i-1]); //Math.sqrt(2*diffusioncoefficient*timelag));
 			t.add(pos);
 		}
-		
+
 		return t;
 	}
 	
@@ -74,36 +73,7 @@ public class AnomalousDiffusionWMSimulation extends AbstractSimulator {
 		
 		return increments;
 	}
-	
-	public double[] generateSteplengths2(){
-		double gamma = Math.sqrt(Math.PI);
-		double H = alpha/2;
-		double[] wxs = new double[numberOfSteps];
-		double[] wys = new double[numberOfSteps];
-		double[] steplengths = new double[numberOfSteps];
 
-		for(int t = 1; t <= numberOfSteps; t++){
-			double tStar = 2*Math.PI*  ((1.0*t)/ (numberOfSteps));
-			double wx = 0;
-			double wy = 0;
-			
-			for(int n = -8; n <= 48; n++){
-				double phasex = CentralRandomNumberGenerator.getInstance().nextDouble()*2*Math.PI;	
-				wx += (Math.cos(phasex)-Math.cos(Math.pow(gamma, n) * tStar + phasex))/Math.pow(gamma, n*H);
-				
-				double phasey = CentralRandomNumberGenerator.getInstance().nextDouble()*2*Math.PI;
-				wy += (Math.cos(phasey)-Math.cos(Math.pow(gamma, n) * tStar + phasey))/Math.pow(gamma, n*H);
-			}
-			double prevwx = (t-2)>=0?wxs[t-2]:0; //Wenn t-2 >= 0 dann gib mir das ELement wxs[t-2], ansonsten 0
-			double prevwy = (t-2)>=0?wys[t-2]:0;
-			wxs[t-1] = wx;
-			wys[t-1] = wy;
-			
-			//steplengths[t-1] = Math.sqrt(Math.pow(wxs[t-1]-prevwx,2)+Math.pow(wys[t-1]-prevwy,2));
-		}
-		
-		return steplengths;
-	}
 	
 	
 	
