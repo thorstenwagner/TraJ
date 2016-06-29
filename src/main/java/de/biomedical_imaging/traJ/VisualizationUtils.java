0795a2b56945c0fee55432ae356c7a3598dd59b3
jumpfunky <thotho@posteo.de>
2016-06-29 13:35:02 +0200

    Add license information and documentation

--- src/main/java/de/biomedical_imaging/traJ/VisualizationUtils.java
+++ src/main/java/de/biomedical_imaging/traJ/VisualizationUtils.java
@@ -1,3 +1,27 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Thorsten Wagner (wagner@biomedical-imaging.de)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package de.biomedical_imaging.traJ;
 
 import java.util.ArrayList;
@@ -32,13 +56,22 @@ public class VisualizationUtils {
 		    new SwingWrapper(chart).displayChart();
 		} 
 	}
+	
 	/**
-	 * Plots the trajectory
+	 * Plots the trajectory t
+	 * @param t Trajectory to be plotted
 	 */
 	public static void showTrajectory(Trajectory t){
 		showTrajectory("No Title", t);
 	}
 
+	/**
+	 * Plots the MSD curve for trajectory t
+	 * @param t Trajectory to calculate the msd curve
+	 * @param lagMin Minimum timelag (e.g. 1,2,3..) lagMin*timelag = elapsed time in seconds 
+	 * @param lagMax Maximum timelag (e.g. 1,2,3..) lagMax*timelag = elapsed time in seconds 
+	 * @param msdeval Evaluates the mean squared displacment
+	 */
 	public static void plotMSDLine(Trajectory t, int lagMin, int lagMax,
 			AbstractMeanSquaredDisplacmentEvaluator msdeval) {
 
@@ -61,6 +94,17 @@ public class VisualizationUtils {
 		new SwingWrapper(chart).displayChart();
 	}
 
+	/**
+	 * Plots the MSD curve with the trajectory t and adds the fitted model for confined diffusion above.
+	 * @param t Trajectory to calculate the msd curve
+	 * @param lagMin Minimum timelag (e.g. 1,2,3..) lagMin*timelag = elapsed time in seconds 
+	 * @param lagMax Maximum timelag (e.g. 1,2,3..) lagMax*timelag = elapsed time in seconds 
+	 * @param timelag Elapsed time between two frames.
+	 * @param a Parameter alpha
+	 * @param b Shape parameter 1
+	 * @param c Shape parameter 2
+	 * @param d Diffusion coefficient
+	 */
 	public static void plotMSDLineWithConfinedModel(Trajectory t, int lagMin,
 			int lagMax, double timelag, double a, double b, double c, double d) {
 
@@ -88,7 +132,16 @@ public class VisualizationUtils {
 		// Show it
 		new SwingWrapper(chart).displayChart();
 	}
-
+	
+	/**
+	 * Plots the MSD curve with the trajectory t and adds the fitted model for anomalous diffusion above.
+	 * @param t
+	 * @param lagMin Minimum timelag (e.g. 1,2,3..) lagMin*timelag = elapsed time in seconds 
+	 * @param lagMax lagMax Maximum timelag (e.g. 1,2,3..) lagMax*timelag = elapsed time in seconds 
+	 * @param timelag Elapsed time between two frames.
+	 * @param a Exponent alpha of power law function
+	 * @param D Diffusion coeffcient 
+	 */
 	public static void plotMSDLineWithPowerModel(Trajectory t, int lagMin,
 			int lagMax, double timelag, double a, double D) {
 
@@ -116,7 +169,14 @@ public class VisualizationUtils {
 		new SwingWrapper(chart).displayChart();
 	}
 
-	public static void plotMSDLine(ArrayList<Trajectory> t, int lagMin,
+	/**
+	 * Plots the mean MSD curve over all trajectories in t.
+	 * @param t List of trajectories
+	 * @param lagMin Minimum timelag (e.g. 1,2,3..) lagMin*timelag = elapsed time in seconds 
+	 * @param lagMax Maximum timelag (e.g. 1,2,3..) lagMax*timelag = elapsed time in seconds 
+	 * @param msdeval Evaluates the mean squared displacment
+	 */
+	public static void plotMSDLine(ArrayList<? extends Trajectory> t, int lagMin,
 			int lagMax, AbstractMeanSquaredDisplacmentEvaluator msdeval) {
 
 		double[] xData = new double[lagMax - lagMin + 1];
@@ -145,11 +205,23 @@ public class VisualizationUtils {
 		new SwingWrapper(chart).displayChart();
 	}
 
+	/**
+	 * Plots the  MSD curve for trajectory t.
+	 * @param t List of trajectories
+	 * @param lagMin Minimum timelag (e.g. 1,2,3..) lagMin*timelag = elapsed time in seconds 
+	 * @param lagMax Maximum timelag (e.g. 1,2,3..) lagMax*timelag = elapsed time in seconds 
+	 */
 	public static void plotMSDLine(Trajectory t, int lagMin, int lagMax) {
 		plotMSDLine(t, lagMin, lagMax, new MeanSquaredDisplacmentFeature(t,
 				lagMin));
 	}
 
+	/**
+	 * Plots the mean MSD curve over all trajectories in t.
+	 * @param t List of trajectories
+	 * @param lagMin Minimum timelag (e.g. 1,2,3..) lagMin*timelag = elapsed time in seconds 
+	 * @param lagMax Maximum timelag (e.g. 1,2,3..) lagMax*timelag = elapsed time in seconds 
+	 */
 	public static void plotMSDLine(ArrayList<Trajectory> t, int lagMin,
 			int lagMax) {
 		plotMSDLine(t, lagMin, lagMax,
