047b7017add6162dc2dea586627686acac7d3591
jumpfunky <thotho@posteo.de>
2016-12-28 12:47:40 +0100

    * Add static method to calcuate radius for a given boundedness * Add documentation

--- src/main/java/de/biomedical_imaging/traJ/features/BoundednessFeature.java
+++ src/main/java/de/biomedical_imaging/traJ/features/BoundednessFeature.java
@@ -24,11 +24,34 @@ public class BoundednessFeature extends AbstractTrajectoryFeature {
 		result = new double[]{res};
 		return result;
 	}
+	
+	/**
+	 * Calculates the Boundedness value to given confinement radius, diffusion coefficient, timlag and number of steps.
+	 * @param D diffusion coefficient
+	 * @param N Number of steps
+	 * @param timelag Timelag
+	 * @param confRadius Confinement radius
+	 * @return Boundedness value
+	 */
 	public static double calculateBoundedness(double D, int N, double timelag, double confRadius){
 		double r = confRadius;
 		double cov_area = a(N)*D*timelag;
 		double res = cov_area/(4*r*r);
 		return res;
+	}
+	
+	/**
+	 * Calculates the radius to a given boundedness value
+	 * @param D Diffusion coefficient
+	 * @param N Number of steps
+	 * @param timelag Timelag
+	 * @param B Boundedeness
+	 * @return Confinement radius
+	 */
+	public static double getRadiusToBoundedness(double D, int N, double timelag, double B){
+		double cov_area = a(N)*D*timelag;
+		double radius = Math.sqrt(cov_area/(4*B));
+		return radius;
 	}
 	public static double a(int length){
 		return 4*length;//-79.751+4.051*length;
